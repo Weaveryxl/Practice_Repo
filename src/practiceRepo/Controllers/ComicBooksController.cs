ecafe26ac55366b124062bc2dad6b8d7b314835b
Weaveryxl <weaveryxl549@gmail.com>
2016-12-30 12:29:04 -0800

    Added detail action method

--- src/practiceRepo/Controllers/ComicBooksController.cs
+++ src/practiceRepo/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace practiceRepo.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
     }
 }
