24e4fb5a41470a72ef45a9dda5587c3dd0f3b4c2
Weaveryxl <weaveryxl549@gmail.com>
2016-12-30 13:13:30 -0800

    Updated Detail action method to use action result types

--- src/practiceRepo/Controllers/ComicBooksController.cs
+++ src/practiceRepo/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace practiceRepo.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
-        {
-            return "Hello from the comic books controller";
+        public ActionResult Detail()
+        {   
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller"
+            };
         }
     }
 }
