1ecc203136a25187fe0a1afdc316a83e46de3229
Weaveryxl <weaveryxl549@gmail.com>
2017-01-30 14:38:49 -0500

    Added comic and artist models

---
+++ src/practiceRepo/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practiceRepo.Models
+{
+    public class ComicBook
+    {
+        public int Id { set; get; }
+        public string SeriesTitle { set; get; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string[] Artists { get; set; }
+        public bool Favorite { set; get; }
+
+    }
+}
---
+++ src/practiceRepo/Models/artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practiceRepo.Models
+{
+    public class artist
+    {
+        public string Name { set; get; }
+        public string Role { get; set; }
+    }
+}
